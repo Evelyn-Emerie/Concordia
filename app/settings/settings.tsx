2f777f3197a6269607991a4f45df57ebff088cec
StarlitEvelyn <evelyn.rei@protonmail.com>
2024-08-20 13:00:09 +0200

    Fixed arrow back and settings path

---
+++ app/settings/settings.tsx
@@ -0,0 +1,134 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AccountSettingsPage from "./account";
+import TestPage from "./test";
+
+const settings = [
+	{
+		title: "User Settings",
+		settings: [
+			{
+				index: 0,
+				title: "Account",
+				view: <AccountSettingsPage key={0} />,
+			},
+			{
+				index: 1,
+				title: "Super Test",
+				view: <TestPage key={1} />,
+			},
+		],
+	},
+];
+
+export default function SettingsScreen() {
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const mainWindow = (selectedIndex: number) => {
+		return settings[0].settings[selectedIndex].view;
+	};
+	return (
+		<GestureHandlerRootView>
+			<SafeAreaView
+				style={{
+					backgroundColor: "#161616",
+					flex: 1,
+					flexDirection: "row",
+				}}>
+				<View>
+					<View style={{ marginLeft: 20, marginTop: 20 }}>
+						<ReturnArrow />
+					</View>
+					<View style={{ marginTop: 30, marginLeft: 10, marginRight: 20 }}>
+						<SettingsList selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+					</View>
+				</View>
+				{mainWindow(selectedIndex)}
+			</SafeAreaView>
+		</GestureHandlerRootView>
+	);
+}
+
+type Setting = {
+	index: number;
+	title: string;
+	view: React.JSX.Element;
+};
+
+const SettingsList = (props: { selectedIndex: number; setSelectedIndex: Function }) => {
+	return (
+		<FlatList
+			data={settings}
+			renderItem={(item) => {
+				return <SettingsCategory key={item.index} title={item.item.title} settings={item.item.settings} selectedIndex={props.selectedIndex} setSelected={props.setSelectedIndex} />;
+			}}
+		/>
+	);
+};
+
+const SettingsCategory = (props: { title: string; settings: Setting[]; selectedIndex: number; setSelected: Function }) => {
+	return (
+		<View style={{ width: 150, paddingHorizontal: 10 }}>
+			<Text
+				style={{
+					color: "#999",
+					fontWeight: "600",
+					marginBottom: 5,
+				}}>
+				{props.title}
+			</Text>
+			{props.settings.map((item, index) => {
+				return <SettingsButton key={item.index} title={item.title} index={item.index} selected={item.index == props.selectedIndex} setSelected={props.setSelected} />;
+			})}
+		</View>
+	);
+};
+
+const SettingsButton = (props: { title: string; index: number; selected: boolean; setSelected: Function }) => {
+	const [hover, SetHover] = useState(false);
+	return (
+		<Pressable
+			onHoverIn={() => {
+				SetHover(true);
+			}}
+			onHoverOut={() => {
+				SetHover(false);
+			}}
+			onPress={() => {
+				props.setSelected(props.index);
+			}}>
+			<View
+				style={{
+					backgroundColor: props.selected ? "#FFFFFF44" : hover ? "#FFFFFF22" : "transparent",
+					width: "100%",
+					paddingHorizontal: 10,
+					borderRadius: 5,
+					marginVertical: 2,
+				}}>
+				<Text style={{ color: "white", marginVertical: 2 }}>{props.title}</Text>
+			</View>
+		</Pressable>
+	);
+};
+
+const ReturnArrow = () => {
+	return (
+		<Link href={"/"}>
+			<View
+				style={{
+					width: 30,
+					height: 30,
+					borderRadius: 30,
+					borderColor: "white",
+					borderWidth: 2,
+					justifyContent: "center",
+					alignItems: "center",
+				}}>
+				<MaterialIcons name="arrow-back-ios-new" size={20} color={"white"} />
+			</View>
+		</Link>
+	);
+};
--- components/ServerList.tsx
+++ components/ServerList.tsx
@@ -1,192 +1,162 @@
-import {
-  EvilIcons,
-  FontAwesome6,
-  Ionicons,
-  MaterialIcons,
-} from "@expo/vector-icons";
+import { EvilIcons, FontAwesome6, Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { Link, useRouter } from "expo-router";
 import { useState, useRef, useEffect } from "react";
 import { View, Animated, Easing, Pressable, Text, Image } from "react-native";
 
 type Server = {
-  id: string;
-  name: string;
-  iconURI?: string;
+	id: string;
+	name: string;
+	iconURI?: string;
 };
 
 export default function ServerList() {
-  const [selectedId, setSelectedId] = useState("1");
-  const serverList: Server[] = [
-    {
-      id: "1",
-      name: "A real server!",
-    },
-    {
-      id: "2",
-      name: "SuperCool Server!",
-    },
-  ];
+	const [selectedId, setSelectedId] = useState("1");
+	const serverList: Server[] = [
+		{
+			id: "1",
+			name: "A real server!",
+		},
+		{
+			id: "2",
+			name: "SuperCool Server!",
+		},
+	];
 
-  const handleServerSelect = (id: string) => {
-    setSelectedId(id);
-  };
+	const handleServerSelect = (id: string) => {
+		setSelectedId(id);
+	};
 
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <View style={{ marginHorizontal: 5 }}>
-      {serverList.map((server) => {
-        return (
-          <ServerIcon
-            key={server.id}
-            onPressed={handleServerSelect}
-            id={server.id}
-            selected={server.id == selectedId}
-            iconURI={server.iconURI}
-            name={server.name}
-          />
-        );
-      })}
-      <AddServer />
-      <View style={{ flex: 1 }} />
-      <Pressable
-        onPress={() => {
-          router.push("/settings");
-        }}
-      >
-        <View
-          style={{
-            width: 40,
-            height: 40,
-            marginBottom: 10,
-          }}
-        >
-          <MaterialIcons name="settings" size={40} color={"#ddd"} />
-        </View>
-      </Pressable>
-    </View>
-  );
+	return (
+		<View style={{ marginHorizontal: 5 }}>
+			{serverList.map((server) => {
+				return <ServerIcon key={server.id} onPressed={handleServerSelect} id={server.id} selected={server.id == selectedId} iconURI={server.iconURI} name={server.name} />;
+			})}
+			<AddServer />
+			<View style={{ flex: 1 }} />
+			<Pressable
+				onPress={() => {
+					router.push("/settings/settings");
+				}}>
+				<View
+					style={{
+						width: 40,
+						height: 40,
+						marginBottom: 10,
+					}}>
+					<MaterialIcons name="settings" size={40} color={"#ddd"} />
+				</View>
+			</Pressable>
+		</View>
+	);
 }
 
 interface ServerIconProps {
-  id?: string;
-  name: string;
-  iconURI?: string;
-  selected?: boolean;
-  onPressed?: Function;
+	id?: string;
+	name: string;
+	iconURI?: string;
+	selected?: boolean;
+	onPressed?: Function;
 }
 
 function ServerIcon(props: ServerIconProps) {
-  const [hover, setHover] = useState(false);
+	const [hover, setHover] = useState(false);
 
-  const borderRadius = useRef(
-    new Animated.Value(props.selected ? 5 : 20)
-  ).current;
+	const borderRadius = useRef(new Animated.Value(props.selected ? 5 : 20)).current;
 
-  const animateBorderRadius = (toValue: number) => {
-    Animated.timing(borderRadius, {
-      toValue,
-      duration: 100, // Adjust the duration as needed
-      easing: Easing.inOut(Easing.ease),
-      useNativeDriver: false, // Border radius does not support native driver
-    }).start();
-  };
+	const animateBorderRadius = (toValue: number) => {
+		Animated.timing(borderRadius, {
+			toValue,
+			duration: 100, // Adjust the duration as needed
+			easing: Easing.inOut(Easing.ease),
+			useNativeDriver: false, // Border radius does not support native driver
+		}).start();
+	};
 
-  useEffect(() => {
-    animateBorderRadius(props.selected ? 5 : 20);
-  }, [props.selected]);
+	useEffect(() => {
+		animateBorderRadius(props.selected ? 5 : 20);
+	}, [props.selected]);
 
-  return (
-    <Pressable
-      onPress={() => {
-        props.onPressed ? props.onPressed(props.id) : null;
-      }}
-      onPointerEnter={() => {
-        setHover(true);
-        animateBorderRadius(props.selected ? 5 : 10);
-      }}
-      onPointerLeave={() => {
-        setHover(false);
-        animateBorderRadius(props.selected ? 5 : 20);
-      }}
-    >
-      {hover ? (
-        <View
-          style={{
-            position: "absolute",
-            left: 65,
-            height: 50, //! Height + MarginTop * 2 === Height Of Server Icon
-            marginTop: 5,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#101010",
-            borderRadius: 10,
-            maxWidth: 150,
-            paddingHorizontal: 10,
-          }}
-        >
-          <View
-            style={{
-              position: "absolute",
-              left: -10, // Position the triangle to the left of the view
-              width: 0,
-              height: 0,
-              borderTopWidth: 10,
-              borderTopColor: "transparent",
-              borderBottomWidth: 10,
-              borderBottomColor: "transparent",
-              borderRightWidth: 10,
-              borderRightColor: "#101010",
-              top: "50%",
-              marginTop: -5, // Center the triangle vertically
-            }}
-          />
-          <Text
-            style={{ color: "white", flexWrap: "nowrap" }}
-            numberOfLines={1}
-            ellipsizeMode="tail"
-          >
-            {props.name}
-          </Text>
-        </View>
-      ) : null}
-      <Animated.View
-        style={{
-          width: 50,
-          height: 50,
-          marginVertical: 5,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#323232",
-          borderRadius: borderRadius, // Use Animated.Value for smooth transitions
-        }}
-      >
-        {props.iconURI ? (
-          <Image source={{ uri: props.iconURI }} />
-        ) : (
-          <Ionicons name="code" color={"white"} size={25} />
-        )}
-      </Animated.View>
-    </Pressable>
-  );
+	return (
+		<Pressable
+			onPress={() => {
+				props.onPressed ? props.onPressed(props.id) : null;
+			}}
+			onPointerEnter={() => {
+				setHover(true);
+				animateBorderRadius(props.selected ? 5 : 10);
+			}}
+			onPointerLeave={() => {
+				setHover(false);
+				animateBorderRadius(props.selected ? 5 : 20);
+			}}>
+			{hover ? (
+				<View
+					style={{
+						position: "absolute",
+						left: 65,
+						height: 50, //! Height + MarginTop * 2 === Height Of Server Icon
+						marginTop: 5,
+						justifyContent: "center",
+						alignItems: "center",
+						backgroundColor: "#101010",
+						borderRadius: 10,
+						maxWidth: 150,
+						paddingHorizontal: 10,
+					}}>
+					<View
+						style={{
+							position: "absolute",
+							left: -10, // Position the triangle to the left of the view
+							width: 0,
+							height: 0,
+							borderTopWidth: 10,
+							borderTopColor: "transparent",
+							borderBottomWidth: 10,
+							borderBottomColor: "transparent",
+							borderRightWidth: 10,
+							borderRightColor: "#101010",
+							top: "50%",
+							marginTop: -5, // Center the triangle vertically
+						}}
+					/>
+					<Text style={{ color: "white", flexWrap: "nowrap" }} numberOfLines={1} ellipsizeMode="tail">
+						{props.name}
+					</Text>
+				</View>
+			) : null}
+			<Animated.View
+				style={{
+					width: 50,
+					height: 50,
+					marginVertical: 5,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "#323232",
+					borderRadius: borderRadius, // Use Animated.Value for smooth transitions
+				}}>
+				{props.iconURI ? <Image source={{ uri: props.iconURI }} /> : <Ionicons name="code" color={"white"} size={25} />}
+			</Animated.View>
+		</Pressable>
+	);
 }
 
 function AddServer() {
-  return (
-    <Pressable onPress={() => {}}>
-      <View
-        style={{
-          width: 50,
-          height: 50,
-          marginVertical: 5,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#323232",
-          borderRadius: 5,
-        }}
-      >
-        <Ionicons name="add-circle-outline" color={"#84ff99"} size={30} />
-      </View>
-    </Pressable>
-  );
+	return (
+		<Pressable onPress={() => {}}>
+			<View
+				style={{
+					width: 50,
+					height: 50,
+					marginVertical: 5,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "#323232",
+					borderRadius: 5,
+				}}>
+				<Ionicons name="add-circle-outline" color={"#84ff99"} size={30} />
+			</View>
+		</Pressable>
+	);
 }
