a91496dc0eb1ce96bb79ec8f0006525d5941b2df
StarlitEvelyn <evelyn.rei@protonmail.com>
2024-08-26 18:31:41 +0200

    Updated all packages (?)

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,11 +1,17 @@
 @echo off
 
+echo Checking packages...
+call npm i
+
 echo Compiling app to static
 call npx expo export --output-dir ./electron/dist --platform web >NUL 2>&1
 
 
-echo Compiling app to .exe package
 cd electron
+echo Checking packages
+call npm i
+
+echo Compiling app to .exe package
 call npx electron-packager . Concordia --platform=win32,linux,darwin --arch=x64 --overwrite --out ".\output" >NUL 2>&1
 cd ..
 
