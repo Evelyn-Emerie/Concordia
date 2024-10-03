26228ccf2884a6519bb0c32524aa3c8f172dac89
StarlitEvelyn <evelyn.rei@protonmail.com>
2024-10-03 16:27:01 +0200

    temporarily removed pfp from usercard till I decide on how to have it work

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -8,7 +8,7 @@ import T_User from "../types/user";
 export default function UserCard(props: { user?: T_User }) {
 	return (
 		<View style={{ alignSelf: "flex-end", width: "100%", paddingHorizontal: 10, height: 60, backgroundColor: Colors.dark.background, flexDirection: "row", justifyContent: "flex-start", alignItems: "center", paddingBottom: 10 }}>
-			<ExpoImage source="https://media1.tenor.com/m/tCL3HGcaV4UAAAAd/raccoon-dance.gif" style={{ width: 45, height: 45 }} />
+			{/* <ExpoImage source="https://media1.tenor.com/m/tCL3HGcaV4UAAAAd/raccoon-dance.gif" style={{ width: 45, height: 45 }} /> */}
 			<View style={{ width: 10 }} />
 			<Text style={{ color: Colors.dark.text, fontWeight: 600, fontSize: 18 }}>{props.user?.username ?? ""}</Text>
 			<View style={{ flex: 1 }} />
